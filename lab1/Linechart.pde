b9ed0b7b752f3babbb310ec471ade63ce36eaa0e
Louis Rassaby <louis.rassaby@gmail.com>
2014-09-13 15:26:15 -0400

    finished lab finally

--- lab1/Linechart.pde
+++ lab1/Linechart.pde
@@ -3,8 +3,21 @@ public class Linechart {
     Point origin, topyaxis, rightxaxis;
     FruitCount[] datapoints;
     boolean isect;
+    int radius = 5;
 
-    String intersect (int x, int y) {
+    String intersect (int mousex, int mousey) {
+        int ratio = (topyaxis.y - origin.y) / maxY;
+        int sectionWidth = abs(((rightxaxis.x - origin.x) / datapoints.length));
+        strokeWeight(sectionWidth * 0.8);
+        
+        for (int i = 0; i < datapoints.length; i++) {
+            int x = origin.x + sectionWidth * i + sectionWidth / 2 + int(sectionWidth * 0.1);
+            int y = datapoints[i].count * ratio + origin.y + 4;
+            
+            if ((mousex - x) * (mousex - x) + (mousey - y) * (mousey - y) < (radius + 1) * (radius + 1)) {
+                return "(" + datapoints[i].fruit + ", " + datapoints[i].count + ")";
+            }
+        }
         return "";
     }
 
@@ -29,11 +42,17 @@ public class Linechart {
         int ratio = (topyaxis.y - origin.y) / maxY;
         int sectionWidth = abs(((rightxaxis.x - origin.x) / datapoints.length));
         Point prev = new Point(origin.x + sectionWidth / 2, datapoints[0].count * ratio + origin.y);
+        drawCircle(prev.x, prev.y, 10);
         for (int i = 1; i < datapoints.length; i++) {
             int x = origin.x + sectionWidth * i + sectionWidth / 2 + int(sectionWidth * 0.1);
             int y = datapoints[i].count * ratio + origin.y;
             line(prev.x, prev.y, x, y);
             prev.setXY(x, y);
+            drawCircle(prev.x, prev.y, radius * 2);
         }
+    }
+
+    void drawCircle(int x, int y, float diameter) {
+        ellipse(x, y, diameter, diameter);
     }
 };
--- lab1/build-tmp/source/lab1.java
+++ lab1/build-tmp/source/lab1.java
@@ -407,8 +407,21 @@ public class Linechart {
     Point origin, topyaxis, rightxaxis;
     FruitCount[] datapoints;
     boolean isect;
+    int radius = 5;
 
-    public String intersect (int x, int y) {
+    public String intersect (int mousex, int mousey) {
+        int ratio = (topyaxis.y - origin.y) / maxY;
+        int sectionWidth = abs(((rightxaxis.x - origin.x) / datapoints.length));
+        strokeWeight(sectionWidth * 0.8f);
+        
+        for (int i = 0; i < datapoints.length; i++) {
+            int x = origin.x + sectionWidth * i + sectionWidth / 2 + PApplet.parseInt(sectionWidth * 0.1f);
+            int y = datapoints[i].count * ratio + origin.y + 4;
+            
+            if ((mousex - x) * (mousex - x) + (mousey - y) * (mousey - y) < (radius + 1) * (radius + 1)) {
+                return "(" + datapoints[i].fruit + ", " + datapoints[i].count + ")";
+            }
+        }
         return "";
     }
 
@@ -433,12 +446,18 @@ public class Linechart {
         int ratio = (topyaxis.y - origin.y) / maxY;
         int sectionWidth = abs(((rightxaxis.x - origin.x) / datapoints.length));
         Point prev = new Point(origin.x + sectionWidth / 2, datapoints[0].count * ratio + origin.y);
+        drawCircle(prev.x, prev.y, 10);
         for (int i = 1; i < datapoints.length; i++) {
             int x = origin.x + sectionWidth * i + sectionWidth / 2 + PApplet.parseInt(sectionWidth * 0.1f);
             int y = datapoints[i].count * ratio + origin.y;
             line(prev.x, prev.y, x, y);
             prev.setXY(x, y);
+            drawCircle(prev.x, prev.y, radius * 2);
         }
+    }
+
+    public void drawCircle(int x, int y, float diameter) {
+        ellipse(x, y, diameter, diameter);
     }
 };
   static public void main(String[] passedArgs) {
