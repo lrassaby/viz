5cf20c8ff57babd9bfb922a89f4bc89f94cd3db4
Louis Rassaby <louis.rassaby@gmail.com>
2014-10-18 19:33:01 -0400

    add gui

--- a3/a3.pde
+++ a3/a3.pde
@@ -1,10 +1,29 @@
+import javax.swing.*;
 NodeSystem diagram;
 
 void setup() {
     frame.setResizable(true);
     size(1000, 800);
     frameRate(60);
-    diagram = new NodeSystem("data.csv");
+
+    String filename = null;
+    try { 
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) { 
+    e.printStackTrace();
+    } 
+
+    try {
+      filename = JOptionPane.showInputDialog(frame, "Input file", "data.csv");
+    } catch (Exception e) {
+      println("Process cancelled.");
+      exit();
+    }
+    if (filename == null) {
+      println("Process cancelled.");
+      exit();
+    }
+    diagram = new NodeSystem(filename);
 }
 
 void draw() {
--- a3/build-tmp/source/a3.java
+++ a3/build-tmp/source/a3.java
@@ -3,6 +3,8 @@ import processing.data.*;
 import processing.event.*; 
 import processing.opengl.*; 
 
+import javax.swing.*; 
+
 import java.util.HashMap; 
 import java.util.ArrayList; 
 import java.io.File; 
@@ -14,13 +16,32 @@ import java.io.IOException;
 
 public class a3 extends PApplet {
 
+
 NodeSystem diagram;
 
 public void setup() {
     frame.setResizable(true);
     size(1000, 800);
     frameRate(60);
-    diagram = new NodeSystem("data.csv");
+
+    String filename = null;
+    try { 
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) { 
+    e.printStackTrace();
+    } 
+
+    try {
+      filename = JOptionPane.showInputDialog(frame, "Input file", "data.csv");
+    } catch (Exception e) {
+      println("Process cancelled.");
+      exit();
+    }
+    if (filename == null) {
+      println("Process cancelled.");
+      exit();
+    }
+    diagram = new NodeSystem(filename);
 }
 
 public void draw() {
