ca038261aeec950026d8c3bbf48f04d7ac5fe244
Louis Rassaby <louis.rassaby@gmail.com>
2014-11-03 20:47:33 -0500

    about to merge with jim's changes

--- a4/build-tmp/source/a4.java
+++ a4/build-tmp/source/a4.java
@@ -169,17 +169,6 @@ public class Barchart extends AxisChart {
         topyaxis.setXY(margins[0], margins[1]);
         rightxaxis.setXY(width - margins[2], height - margins[3]);
         float c = 0;
-        /*switch(transition) {
-            case 0:
-            case LINETOBAR:
-            case BARTOLINE:
-                c = 0;
-                break;
-            case BARTOPIE:
-            case PIETOBAR:
-                c = serp(255, 0, transition_completeness);
-                break;
-        }*/
         int col = color(c, c, c);
         drawAxes(col);
         drawLabels(col, PApplet.parseFloat(origin.y - topyaxis.y) / maxY);
@@ -197,31 +186,16 @@ public class Barchart extends AxisChart {
         switch(transition) {
             case 0:
                 for (int i = 0; i < categories.length; i++) {
-                    if (selected[i] == 1) {stroke(13, 134, 90, 255);}
-                    else {stroke(5, 112, 204, 255);}
                     int x = origin.x + sectionWidth * i + sectionWidth / 2 + PApplet.parseInt(sectionWidth * 0.1f);
                     int y = origin.y - PApplet.parseInt(data.getRow(0).getInt(categories[i]) * ratio);
+                    selected[i] = selected[i] == 1 ? 1 : PApplet.parseInt(mouseX >= x - sectionWidth/2 && mouseX <= x + sectionWidth/2 && mouseY >= y && mouseY <= origin.y);
+                    if (selected[i] == 1) {stroke(13, 134, 90, 255);}
+                    else {stroke(5, 112, 204, 255);}
                     line(x, origin.y, x, y);
                     intersections[2*i] = x;
                     intersections[2*i + 1] = y;
                 }
                 break;
-            /*case LINETOBAR:
-            case BARTOLINE:
-                for (int i = 0; i < data.getRowCount(); i++) {
-                    int x = origin.x + sectionWidth * i + sectionWidth / 2 + int(sectionWidth * 0.1);
-                    int y = origin.y - int(data.getRow(i).getInt(categories[1]) * ratio);
-                    line(x, serp(y, origin.y, transition_completeness), x, y);
-                }
-                break;
-            case BARTOPIE:
-            case PIETOBAR:
-                for (int i = 0; i < data.getRowCount(); i++) {
-                    int x = origin.x + sectionWidth * i + sectionWidth / 2 + int(sectionWidth * 0.1);
-                    int y = origin.y - int(data.getRow(i).getInt(categories[1]) * ratio);
-                    line(x, origin.y, x, y);
-                }
-                break;*/
         }
         
     }   
